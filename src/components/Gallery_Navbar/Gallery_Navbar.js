b1e25998ed918ca6a1401067a222407fa2ce2336
mayanmilis <mayanmilis16@gmail.com>
2019-12-11 07:44:00 +0200

    gallery nav

--- src/components/Gallery_Navbar/Gallery_Navbar.js
+++ src/components/Gallery_Navbar/Gallery_Navbar.js
@@ -13,9 +13,9 @@ class GalleryNavbar extends Component {
             <nav className="gallery_navbar_container">
                 <div className="logo">BARBER SHOP</div>
                 <ul>
-                    <a href="http://localhost:3000/gallery/men"><li className={ category==="men" ? "active" : ""}>Men</li></a>
-                    <a href="http://localhost:3000/gallery/women"><li className={ category==="women" ? "active" : ""}>Women</li></a>
-                    <a href="http://localhost:3000/gallery/products"><li className={ category==="products" ? "active" : ""}>Products</li></a>
+                    <Link to="/gallery/men"><li className={ category==="men" ? "active" : ""}>Men</li></Link>
+                    <Link to="/gallery/women"><li className={ category==="women" ? "active" : ""}>Women</li></Link>
+                    <Link to="/gallery/products"><li className={ category==="products" ? "active" : ""}>Products</li></Link>
                 </ul>
             </nav>
         ) 
